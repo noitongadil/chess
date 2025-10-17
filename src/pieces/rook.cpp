8855a9d0dd6032a418ddb39906d8ed02bc06e551
noitongadil <btrglm@proton.me>
2025-10-17 10:47:04 -0600

    rook reimplemented

--- src/pieces/rook.cpp
+++ src/pieces/rook.cpp
@@ -2,19 +2,114 @@
 #include "board.h"
 #include <string>
 
-Rook::Rook(Board *board, Position pos, Side side)
-    : Piece(board, pos, side, 'R')
+Rook::Rook(Board *board, Side side)
+    : Piece(board, side, 'R')
 {
 }
 
-std::vector<std::string> Rook::possible_moves() const
+std::vector<std::string> Rook::get_moves(int8_t file, int8_t rank) const
 {
-    std::vector<std::string> possible_moves;
+    std::vector<std::string> moves;
 
-    rec_helper(possible_moves, m_pos, "n", true);
-    rec_helper(possible_moves, m_pos, "s", true);
-    rec_helper(possible_moves, m_pos, "e", true);
-    rec_helper(possible_moves, m_pos, "w", true);
+    // up
+    for (int j = rank; j < 8; j++)
+    {
+        if (m_board->m_grid[file][j] != nullptr)
+        {
+            if (m_board->m_grid[file][j]->get_side() != m_side)
+            {
+                std::string move = "R";
+                move += 'x';
+                move += file + 'a';
+                move += std::to_string(j + 1);
+                moves.push_back(move);
+                disambiguate(move, file, j, moves);
+            }
 
-    return possible_moves;
+            break;
+        }
+
+        std::string move = "R";
+        move += file + 'a';
+        move += std::to_string(j + 1);
+        moves.push_back(move);
+        disambiguate(move, file, j, moves);
+    }
+
+    // down
+    for (int j = rank; j >= 0; j--)
+    {
+        if (m_board->m_grid[file][j] != nullptr)
+        {
+            if (m_board->m_grid[file][j]->get_side() != m_side)
+            {
+                std::string move = "R";
+                move += 'x';
+                move += file + 'a';
+                move += std::to_string(j + 1);
+                moves.push_back(move);
+                disambiguate(move, file, j, moves);
+            }
+
+            break;
+        }
+
+        std::string move = "B";
+        move += file + 'a';
+        move += std::to_string(j + 1);
+        moves.push_back(move);
+        disambiguate(move, file, j, moves);
+    }
+
+    // left
+    for (int i = file; i >= 0; i--)
+    {
+        if (m_board->m_grid[i][rank] != nullptr)
+        {
+            if (m_board->m_grid[i][rank]->get_side() != m_side)
+            {
+                std::string move = "R";
+                move += 'x';
+                move += i + 'a';
+                move += std::to_string(rank + 1);
+                moves.push_back(move);
+                disambiguate(move, i, rank, moves);
+            }
+
+            break;
+        }
+
+        std::string move = "R";
+        move += i + 'a';
+        move += std::to_string(rank + 1);
+        moves.push_back(move);
+        disambiguate(move, i, rank, moves);
+    }
+
+    // right
+    for (int i = file; i >= 0; i--)
+    {
+        if (m_board->m_grid[i][rank] != nullptr)
+        {
+            if (m_board->m_grid[i][rank]->get_side() != m_side)
+            {
+                std::string move = "R";
+                move += 'x';
+                move += i + 'a';
+                move += std::to_string(rank + 1);
+                moves.push_back(move);
+                disambiguate(move, i, rank, moves);
+            }
+
+            break;
+        }
+
+        std::string move = "R";
+        move += i + 'a';
+        move += std::to_string(rank + 1);
+        moves.push_back(move);
+        disambiguate(move, i, rank, moves);
+    }
+
+    return moves;
 }
