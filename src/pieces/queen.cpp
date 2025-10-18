f04b6a0fec813586721079b8aa86275684a16a71
noitongadil <btrglm@proton.me>
2025-10-18 14:13:16 -0600

    queen reimplemented

--- src/pieces/queen.cpp
+++ src/pieces/queen.cpp
@@ -1,23 +1,234 @@
 #include "board.h"
 #include "queen.h"
 
-Queen::Queen(Board *board, Position pos, Side side)
-    : Piece(board, pos, side, 'Q')
+Queen::Queen(Board *board, Side side)
+    : Piece(board, side, 'Q')
 {
 }
 
-std::vector<std::string> Queen::possible_moves() const
+std::vector<std::string> Queen::get_moves(int8_t file, int8_t rank) const
 {
-    std::vector<std::string> possible_moves;
+    std::vector<std::string> moves;
 
-    rec_helper(possible_moves, m_pos, "n", true);
-    rec_helper(possible_moves, m_pos, "s", true);
-    rec_helper(possible_moves, m_pos, "e", true);
-    rec_helper(possible_moves, m_pos, "w", true);
-    rec_helper(possible_moves, m_pos, "nw", true);
-    rec_helper(possible_moves, m_pos, "ne", true);
-    rec_helper(possible_moves, m_pos, "sw", true);
-    rec_helper(possible_moves, m_pos, "se", true);
+    // up right
+    for (int i = file; i < 8; i++)
+    {
+        for (int j = rank; j < 8; j++)
+        {
+            if (m_board->m_grid[i][j] != nullptr)
+            {
+                if (m_board->m_grid[i][j]->get_side() != m_side)
+                {
+                    std::string move = "Q";
+                    move += 'x';
+                    move += i + 'a';
+                    move += std::to_string(j + 1);
+                    moves.push_back(move);
+                    disambiguate(move, i, j, moves);
+                }
 
-    return possible_moves;
+                i = 9;
+                j = 9;
+                break;
+            }
+
+            std::string move = "Q";
+            move += i + 'a';
+            move += std::to_string(j + 1);
+            moves.push_back(move);
+            disambiguate(move, i, j, moves);
+        }
+    }
+
+    // up left
+    for (int i = file; i >= 0; i--)
+    {
+        for (int j = rank; j < 8; j++)
+        {
+            if (m_board->m_grid[i][j] != nullptr)
+            {
+                if (m_board->m_grid[i][j]->get_side() != m_side)
+                {
+                    std::string move = "Q";
+                    move += 'x';
+                    move += i + 'a';
+                    move += std::to_string(j + 1);
+                    moves.push_back(move);
+                    disambiguate(move, i, j, moves);
+                }
+
+                i = -1;
+                j = 9;
+                break;
+            }
+
+            std::string move = "Q";
+            move += i + 'a';
+            move += std::to_string(j + 1);
+            moves.push_back(move);
+            disambiguate(move, i, j, moves);
+        }
+    }
+
+    // down right
+    for (int i = file; i < 8; i++)
+    {
+        for (int j = rank; j >= 0; j--)
+        {
+            if (m_board->m_grid[i][j] != nullptr)
+            {
+                if (m_board->m_grid[i][j]->get_side() != m_side)
+                {
+                    std::string move = "Q";
+                    move += 'x';
+                    move += i + 'a';
+                    move += std::to_string(j + 1);
+                    moves.push_back(move);
+                    disambiguate(move, i, j, moves);
+                }
+
+                i = 9;
+                j = -1;
+                break;
+            }
+
+            std::string move = "Q";
+            move += i + 'a';
+            move += std::to_string(j + 1);
+            moves.push_back(move);
+            disambiguate(move, i, j, moves);
+        }
+    }
+
+    // down left
+    for (int i = file; i >= 0; i--)
+    {
+        for (int j = rank; j >= 0; j--)
+        {
+            if (m_board->m_grid[i][j] != nullptr)
+            {
+                if (m_board->m_grid[i][j]->get_side() != m_side)
+                {
+                    std::string move = "Q";
+                    move += 'x';
+                    move += i + 'a';
+                    move += std::to_string(j + 1);
+                    moves.push_back(move);
+                    disambiguate(move, i, j, moves);
+                }
+
+                i = -1;
+                j = -1;
+                break;
+            }
+
+            std::string move = "Q";
+            move += i + 'a';
+            move += std::to_string(j + 1);
+            moves.push_back(move);
+            disambiguate(move, i, j, moves);
+        }
+    }
+
+    // up
+    for (int j = rank; j < 8; j++)
+    {
+        if (m_board->m_grid[file][j] != nullptr)
+        {
+            if (m_board->m_grid[file][j]->get_side() != m_side)
+            {
+                std::string move = "Q";
+                move += 'x';
+                move += file + 'a';
+                move += std::to_string(j + 1);
+                moves.push_back(move);
+                disambiguate(move, file, j, moves);
+            }
+
+            break;
+        }
+
+        std::string move = "Q";
+        move += file + 'a';
+        move += std::to_string(j + 1);
+        moves.push_back(move);
+        disambiguate(move, file, j, moves);
+    }
+
+    // down
+    for (int j = rank; j >= 0; j--)
+    {
+        if (m_board->m_grid[file][j] != nullptr)
+        {
+            if (m_board->m_grid[file][j]->get_side() != m_side)
+            {
+                std::string move = "Q";
+                move += 'x';
+                move += file + 'a';
+                move += std::to_string(j + 1);
+                moves.push_back(move);
+                disambiguate(move, file, j, moves);
+            }
+
+            break;
+        }
+
+        std::string move = "Q";
+        move += file + 'a';
+        move += std::to_string(j + 1);
+        moves.push_back(move);
+        disambiguate(move, file, j, moves);
+    }
+
+    // left
+    for (int i = file; i >= 0; i--)
+    {
+        if (m_board->m_grid[i][rank] != nullptr)
+        {
+            if (m_board->m_grid[i][rank]->get_side() != m_side)
+            {
+                std::string move = "Q";
+                move += 'x';
+                move += i + 'a';
+                move += std::to_string(rank + 1);
+                moves.push_back(move);
+                disambiguate(move, i, rank, moves);
+            }
+
+            break;
+        }
+
+        std::string move = "Q";
+        move += i + 'a';
+        move += std::to_string(rank + 1);
+        moves.push_back(move);
+        disambiguate(move, i, rank, moves);
+    }
+
+    // right
+    for (int i = file; i >= 0; i--)
+    {
+        if (m_board->m_grid[i][rank] != nullptr)
+        {
+            if (m_board->m_grid[i][rank]->get_side() != m_side)
+            {
+                std::string move = "Q";
+                move += 'x';
+                move += i + 'a';
+                move += std::to_string(rank + 1);
+                moves.push_back(move);
+                disambiguate(move, i, rank, moves);
+            }
+
+            break;
+        }
+
+        std::string move = "Q";
+        move += i + 'a';
+        move += std::to_string(rank + 1);
+        moves.push_back(move);
+        disambiguate(move, i, rank, moves);
+    }
+
+    return moves;
 }
